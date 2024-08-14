936e1c3767aabddf92a230ce90903850e67b7a60
Rajesh Narayana Rao <mailrbn@gmail.com>
2024-08-14 08:22:13 +0000

    sp

--- s3/powershell-scripts/s3.ps1
+++ s3/powershell-scripts/s3.ps1
@@ -7,4 +7,25 @@ $bucketName = Read-Host Prompt "Enter the S3 bucket name: "
 Write-Host = "AWS region: $region"
 Write-Host = "s3 Bucket: $bucketName"
 
+function BucketExists {
+    $bucket = Get-S3Bucket -BucketName $bucketName -ErrorAction SilentlyContinue
+    return $null -ne $bucket
+}
+
+if(-not (BucketExists)) {
+    Write-Host = "Bucket does not exists...."
+    New-S3Bucket -BucketName $bucketName -Region $region
+} else {
+    Write-Host = "Bucket already exists...."
+}
+
 New-S3Bucket -BucketName $bucketName -Region $region
+
+# Create a new file
+
+$fileName = "myfilename.tex"
+$fileContent = "Hello World!"
+
+Set-Content -Path $fileName -Value $fileContent
+
+Write-S3Object -BucketName $bucketName -File $fileName -Key $fileName
